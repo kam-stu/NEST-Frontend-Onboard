d3dfdec83089c38274bf34f84f2889879d891440
aintkam <kstuckey817@gmail.com>
2025-03-15 17:14:38 -0500

    Added DB Scheme
    Added a database schema with the users table that has the following:
    id : UUID
    username : unique char
    password_hashed : text
    
    and task table with the following:
    id : uuid
    user_id : UUID from user table
    title : char
    task_description : text
    creation_time : time
    task_status : char ("in progress" or "completed")
    completion_time : time

---
+++ database/schema.sql
@@ -0,0 +1,17 @@
+-- Database: User and Tasks
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password_hashed TEXT NOT NULL
+);
+
+CREATE TABLE tasks (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE, -- for owner of the task
+    title VARCHAR(255) NOT NULL,
+    task_description TEXT,
+    creation_time TIMESTAMP DEFAULT NOW(),
+    task_status VARCHAR(50) CHECK (task_status IN ('in progress', 'completed')) NOT NULL DEFAULT 'in progress',
+    completion_time TIMESTAMP
+);
