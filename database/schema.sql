59df510407ffa08f379b011f351d9589039f5e74
aintkam <kstuckey817@gmail.com>
2025-03-15 18:45:47 -0500

    Added docker-compose and Cargo project for Rust
    - docker-compose file added with a docker image for the PostgreSQL database (and for the backend and frontend in the future)
    - Created a cargo project in /database (for connecting the database using Rust)
    - Added a README.md for the database (to explain how it works / all the contents for the table later)
    - Added a .gitignore file for /database to ignore Cargo files that are created during compilation + execution

--- database/schema.sql
+++ database/schema.sql
@@ -9,7 +9,7 @@ CREATE TABLE users (
 CREATE TABLE tasks (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(), 
     user_id UUID REFERENCES users(id) ON DELETE CASCADE, -- for owner of the task
-    title VARCHAR(255) NOT NULL,
+    task_title VARCHAR(255) NOT NULL,
     task_description TEXT,
     creation_time TIMESTAMP DEFAULT NOW(),
     task_status VARCHAR(50) CHECK (task_status IN ('in progress', 'completed')) NOT NULL DEFAULT 'in progress',
